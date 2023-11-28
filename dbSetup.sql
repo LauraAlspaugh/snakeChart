c52762e71093bd8567fab7f280ce4bb7e633ae9a
Laura <lauraalspaugh2002@icloud.com>
2023-11-28 10:41:09 -0700

    working on db setup.sql

--- dbSetup.sql
+++ dbSetup.sql
@@ -1,8 +1,17 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture'
+    ) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS homes(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'this is the id of a home',
+        name CHAR(255) NOT NULL COMMENT 'this is the home name'
+    ) default charset utf8 COMMENT '';
+
+INSERT INTO homes (name) VALUES('LA Mansion');
